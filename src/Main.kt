74b217111913592f9cd089ee39e430e3fa38c51a
M3NGSZE <mengse.thun13@gmail.com>
2025-08-07 22:11:50 +0700

    section 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Hello world")
+    println(isPositive(1))
+
 }
+
+fun isPositive(number: Int): Boolean {
+    return number > 0
+}
+
+
